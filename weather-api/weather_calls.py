cab618339c37187506ed78ae9329aaf79226b132
pcrespoo <pedrocrey94@gmail.com>
2022-08-31 14:46:34 -0300

    Fix typo

--- weather-api/weather_calls.py
+++ weather-api/weather_calls.py
@@ -17,8 +17,8 @@ print('Iniciando consulta da previsão tempo para os próximos dias...')
 geo_request = requests.get('http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}'.format(city, API_KEY))
 geo_result = geo_request.json()
 lat = geo_result[0]['lat']
-long = geo_result[0]['lon']
-weather_request = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&units=metric'.format(lat,long, API_KEY))
+lon = geo_result[0]['lon']
+weather_request = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&units=metric'.format(lat,lon, API_KEY))
 weather_result = weather_request.json()
 print('Consulta concluída com êxito! Salvando no DB...')
 collection.insert_one(weather_result)
